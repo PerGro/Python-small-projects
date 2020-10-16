d5a8f5ade17df2ef547fe2b62535c421cb5a3445
PerGro <44392593+PerGro@users.noreply.github.com>
2020-10-17 00:56:05 +0800

    Update interfaces.py

--- 配 筋 带 师 1.0/interfaces.py
+++ 配 筋 带 师 1.0/interfaces.py
@@ -3,7 +3,8 @@ import datasets
 from datasets import ENV_B, ENV_L, XDSYGD_JMZDDKJXS, KLQD, KYQD
 from datasets import *
 
-def get_envb(env:str):
+
+def get_envb(env: str):
     """
     获得环境因素下板的最低保护层厚度
     :param env:
@@ -11,7 +12,8 @@ def get_envb(env:str):
     """
     return ENV_B[env]
 
-def get_envl(env:str):
+
+def get_envl(env: str):
     """
     获得环境因素下梁的最低保护层厚度
     :param env:
@@ -19,13 +21,16 @@ def get_envl(env:str):
     """
     return ENV_L[env]
 
-def get_envl_l(env:list):
+
+def get_envl_l(env: list):
     return get_envl(env[0])
 
-def get_envb_l(env:list):
+
+def get_envb_l(env: list):
     return get_envb(env[0])
 
-def get_jmspace(n:int, d:int):
+
+def get_jmspace(n: int, d: int):
     """
     用来获得公称截面面积
     :param n: 为钢筋数量
@@ -41,42 +46,57 @@ def get_jmspace(n:int, d:int):
         _ += 1
     return a[_][n - 1]
 
-def get_xdsygd(h:str, g:str):
+
+def get_xdsygd(h: str, g: str):
     return XDSYGD_JMZDDKJXS[h][g]
 
-def get_xdsygd_l(strings:list):
+
+def get_xdsygd_l(strings: list):
     if int(strings[0][1:]) < 50:
         strings[0] = 'C50'
     return get_xdsygd(strings[0], strings[1])
 
-def get_kyqd(h:str):
+
+def get_kyqd(h: str):
     return KYQD[h]
 
-def get_kyqd_l(strings:list):
+
+def get_kyqd_l(strings: list):
     return get_kyqd(strings[0])
 
-def get_klqd(h:str):
+
+def get_klqd(h: str):
     return KLQD[h]
 
-def get_klqd_l(strings:list):
+
+def get_klqd_l(strings: list):
     return get_klqd(strings[0])
 
-def get_gj_kyqd(g:str):
+
+def get_gj_kyqd(g: str):
     return datasets.GJ_KYQD[g]
 
-def get_gj_kyqd_l(strings:list):
+
+def get_gj_kyqd_l(strings: list):
     return get_gj_kyqd(strings[0])
 
-def get_gj_klqd(g:str):
+
+def get_gj_klqd(g: str):
     return datasets.GJ_KLQD[g]
 
-def get_gj_klqd_l(strings:list):
+
+def get_gj_klqd_l(strings: list):
     return get_gj_klqd(strings[0])
 
-def get_syqylxs(c:str):
+
+def get_syqylxs(c: str):
     return SYQYLXS[c]
 
-def get_syqylxs_l(strings:list):
+
+def get_syqylxs_l(strings: list):
     if int(strings[0][1:]) < 50:
         strings[0] = 'C50'
     return get_syqylxs(strings[0])
+
+def get_all(strings):
+    return get_xdsygd_l(strings), get_kyqd(strings[0]), get_klqd(strings[0]), get_gj_kyqd(strings[1]), get_gj_klqd(strings[1]), get_syqylxs_l(strings)
